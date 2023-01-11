32dc2c9eeba3b4c5d88d43f37bbbe061c42e86f9
salma-anika <anikaavaa@gmail.com>
2023-01-11 18:09:27 +0600

    complete office section

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 	import Navbar from '../components/Navbar.svelte';
     import Hero from '../components/Hero.svelte';
 	import About from '../components/About.svelte';
-	import Office from 'src/components/Office.svelte';
+	import Office from '../components/Office.svelte';
   </script>
       <div class=" w-[100%] overflow-hidden bg-black">
         <div class="px-6 sm:px-16 flex justify-center items-center">
@@ -22,8 +22,8 @@
             <About/>
             </div>
                   </div>
-                  <div class='bg-black flex justify-center items-start'>
-                    <div class='xl:max-w-[1280px] w-full'>
+                  <div class='bg-black flex justify-center items-start mt-[100px] w-[60%] m-auto'>
+                    <div class=' w-[70%]'>
                     <Office/>
                     </div>
                           </div>
