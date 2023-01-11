33aa85b243cab73a0c88b38835e8488455fcee7b
salma-anika <anikaavaa@gmail.com>
2023-01-11 17:26:01 +0600

    create office file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import Navbar from '../components/Navbar.svelte';
     import Hero from '../components/Hero.svelte';
 	import About from '../components/About.svelte';
+	import Office from 'src/components/Office.svelte';
   </script>
       <div class=" w-[100%] overflow-hidden bg-black">
         <div class="px-6 sm:px-16 flex justify-center items-center">
@@ -21,3 +22,8 @@
             <About/>
             </div>
                   </div>
+                  <div class='bg-black flex justify-center items-start'>
+                    <div class='xl:max-w-[1280px] w-full'>
+                    <Office/>
+                    </div>
+                          </div>
