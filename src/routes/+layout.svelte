418dd9e4511aba3fbcb28167a719595008f77300
salma-anika <anikaavaa@gmail.com>
2023-01-11 18:11:51 +0600

    create career section

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,7 @@
     import Hero from '../components/Hero.svelte';
 	import About from '../components/About.svelte';
 	import Office from '../components/Office.svelte';
+	import Career from '../components/Career.svelte';
   </script>
       <div class=" w-[100%] overflow-hidden bg-black">
         <div class="px-6 sm:px-16 flex justify-center items-center">
@@ -27,3 +28,8 @@
                     <Office/>
                     </div>
                           </div>
+                          <div class=' bg-white px-6 sm:px-16 flex justify-center items-start'>
+                            <div class='xl:max-w-[1280px] w-full'>
+                            <Career/>
+                            </div>
+                                  </div>
