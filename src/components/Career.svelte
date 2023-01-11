d383936c22e4e3f6343fc3d1b152d9ffeeaab9b8
salma-anika <anikaavaa@gmail.com>
2023-01-12 02:07:43 +0600

    change in career

--- src/components/Career.svelte
+++ src/components/Career.svelte
@@ -5,10 +5,10 @@
 <section id="office" class="mt-[100px]">
     <div class="">
         <div class="">
-            <h2 class="text-black text-[40px] font-semibold ml-[200px]">A new career lifestyle</h2>
+            <h2 class="text-black text-[40px] font-semibold text-center">A new career lifestyle</h2>
             <p class=" text-gray-500 text-center text-[12px]">Software development methodology is a process or series of processes used in software development. Again,
                 quite broad but that it is things like a design phase, a development phase. </p>
-                <div class="ml-[300px] mt-2">
+                <div class="text-center mt-2">
                     <Button/>
                 </div>
               
