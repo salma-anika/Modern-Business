66abb7b8b6b8b31c46b14adf483ec01d49b02fe1
salma-anika <anikaavaa@gmail.com>
2023-01-11 17:35:23 +0600

    insert image

---
+++ src/components/Office.svelte
@@ -0,0 +1,7 @@
+<section id="office" class="">
+    <div class="">
+        <div class="">
+            <img src="webgl.png" class="mt-10 w-[70%] max-h-[350px] m-auto" alt=""/>
+        </div>
+    </div>
+</section>
