b1c642cbf9777493d223921ef9914f2b1e92c1d0
salma-anika <anikaavaa@gmail.com>
2023-01-13 20:54:24 +0600

    responsive mobile view

--- src/components/Apply.svelte
+++ src/components/Apply.svelte
@@ -1,14 +1,14 @@
-<div class=' justify-start items-start w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer mt-[40px]'>
+<div class=' justify-start items-start w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer mt-[40px] max-xs:w-[70px] max-xs:h-[70px] max-xs:mt-[10px]'>
     <div class='flex justify-center items-center flex-row bg-text-gradient w-[100%] h-[100%] rounded-full'>
         <div class='justify-center items-start '>
             
             <img src="arrow-up.svg" alt="arrow"
-            class="w-[103px] h-[103px] object-contain "/>
+            class="w-[103px] h-[103px] object-contain max-xs:w-[52px] max-xs:h-[52px] "/>
         </div>
        
     </div>
     <div class="mt-5">
-        <p class='font-poppins font-bold text-[18px] leading-[23px] mr-2'>
+        <p class='font-poppins font-bold text-[18px] leading-[23px] mr-2 max-xs:text-[12px] '>
             <span class='text-gradient uppercase'>Apply Today</span>
         </p>
        
--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -5,23 +5,23 @@
 </script>
 <section class='flex md:flex-row flex-col py-6 sm:py-16' id="home">
     <div class='justify-center items-start flex-1 flex-col xl:px-0 sm:px-16 px-6'>
-        <p class='max-w-[470px] mt-5 font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] mb-20'>
+        <p class='max-w-[470px] mt-5 font-poppins font-normal text-dimWhite text-[18px] max-xs:leading-[15px] leading-[30.8px] mb-20 max-xs:mb-5 max-xs:text-[12px]'>
             Software development methodology is a process or series of processes used in software development. Again,
             quite broad but that it is things like a design phase, a development phase. 
            
             </p>
     <div class='flex flex-row justify-between items-center w-full max-xs:flex-col'>
         
-<h1 class='flex-1 font-poppins font-semibold  text-[82px] text-white ss:leading-[100px] leading-[75px] uppercase max-xs:text-[52px]'>
+<h1 class='flex-1 font-poppins font-semibold  text-[82px] text-white ss:leading-[100px] leading-[75px] max-xs:leading-[30px] uppercase max-xs:text-[32px]'>
 Work directly <br class='sm:block hidden'/> {" "}
 <span class='uppercase'>With top us Tech</span>{" "}
 
 </h1>
-<div class='ss:flex hidden justify-center items-start '>
+<div class='ss:flex hidden justify-center items-start'>
     <Apply/>
 </div>
     </div>
-    <h1 class='flex-1 font-poppins font-semibold  text-[82px] text-white ss:leading-[100px] leading-[75px] w-full uppercase max-xs:text-[52px]'>
+    <h1 class='flex-1 font-poppins font-semibold  text-[82px] text-white ss:leading-[100px] max-xs:leading-[30px] leading-[75px] w-full uppercase max-xs:text-[32px]'>
  companies
 </h1>
 <div class='ss:hidden flex justify-center items-start '>
